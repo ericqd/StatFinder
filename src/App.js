69ac2ca363b8ab7398c05ceb8aa38b4673794c39
Eric Dao <eric_dao@hotmail.com>
2020-08-18 11:31:32 -0700

    added graph

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React,{ Component } from 'react';
-import {Form, Card, Image, Icon} from 'semantic-ui-react'
+import {Form, Card, Icon} from 'semantic-ui-react'
 import './App.css';
+import {Line} from 'react-chartjs-2';
 
 class App extends Component {
   constructor(props){
     super(props);
     this.state = {
       playerName: null,
-      playerStats: {}
+      playerStats: {},
+      chartData:{},
     }
   }
 
@@ -22,7 +24,8 @@ class App extends Component {
           alert("Please enter full name of the player you want to search")
         }
         else{
-          this.getPlayerStats(data.data[0].id) 
+          this.getPlayerStats(data.data[0].id)
+          this.getChartData(data.data[0].id)
         }
       })
       .catch(err => {
@@ -39,10 +42,48 @@ class App extends Component {
       })
       .catch(err => console.log(err))
   }
+  getChartData = (playerId) => {
+    var labelArr = [];
+    var dataArr = [];
+    fetch(`https://www.balldontlie.io/api/v1/stats?seasons[]=2019&player_ids[]=${playerId}&start_date='2020-07-30'&end_date='2020-08-14'`)
+    .then(res => res.json())
+    .then(data => {
+      for(let i = 0; i < data.data.length; i++){
+        labelArr.push(data.data[i]["game"]["date"])
+        dataArr.push(data.data[i]["pts"])
+      }
+      })
+    .catch(err => console.log(err))
+    this.setState({
+      chartData: {
+        key: Date.now(),
+        labels: labelArr,
+        datasets:[
+          {
+            label:"Points",
+            fill: false,
+            lineTension: 0.5,
+            data: dataArr,
+            borderColor: "rgba(0,0,0,1)",
+            backgroundColor:[
+              'rgba(255,99,132,0.6)',
+              'rgba(54,162,235,0.6)',
+              'rgba(255,206,86,0.6)',
+              'rgba(75,192,192,0.6)',
+              'rgba(153,159,64,0.6)',
+              'rgba(255,99,132,0.6)'
+            ]
+          }
+        ]
+      }
+    })
+  }
   handleSubmit = (e) => {
     e.preventDefault();
     this.getPlayerId()
     console.log(this.state.playerName)
+    console.log(this.state.playerStats)
+    console.log(this.state.chartData)
   }
   handleSearch = (e) => {
     const split = e.target.value.split(" ").join("_")
@@ -66,7 +107,13 @@ class App extends Component {
             </Form.Group>
           </Form>
         </div>
-        <div className="card">
+        <div className="cardHeader"> 
+          <div>
+            <h3>'19 - '20 Season Averages</h3>
+            <br></br>
+          </div>
+        </div>
+        <div className="card"> 
           <Card>
             <Card.Content extra>
               <a>
@@ -111,6 +158,16 @@ class App extends Component {
               </a>
             </Card.Content>
           </Card>
+        </div>
+        <div className="chart">
+          <h3>Bubble Game Stats</h3>
+        <Line
+          data={this.state.chartData}
+          key={this.state.chartData.key}
+          width={100}
+          height={100}
+          options={{responsive:true}}
+        />
         </div>
       </div>
     );
