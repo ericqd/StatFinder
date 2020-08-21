91b5e657d88f6392cd12817f23e59a8dedb43cc9
Eric Dao <eric_dao@hotmail.com>
2020-08-21 00:07:45 -0700

    dropdown menu

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React,{ Component } from 'react';
-import {Form, Card, Icon} from 'semantic-ui-react'
+import {Form, Card, Icon, Dropdown, Menu} from 'semantic-ui-react'
 import './App.css';
 import {Line} from 'react-chartjs-2';
-
 class App extends Component {
   constructor(props){
     super(props);
@@ -10,10 +9,12 @@ class App extends Component {
       playerName: null,
       playerStats: {},
       chartData:{},
+      playerKey: {}
     }
   }
 
   getPlayerId = () => {
+    const idArr = [];
     fetch(`https://www.balldontlie.io/api/v1/players?search=${this.state.playerName}`)
       .then(res => res.json())
       .then(data => {
@@ -24,13 +25,19 @@ class App extends Component {
           alert("Please enter full name of the player you want to search")
         }
         else{
+          idArr.push(data.data[0].id)
           this.getPlayerStats(data.data[0].id)
-          this.getChartData(data.data[0].id)
+          // this.getChartData(data.data[0].id)
         }
       })
       .catch(err => {
         console.log(err)
       })
+    this.setState({
+      playerKey: {
+        id: idArr
+      }
+    })
   }
   
   getPlayerStats = (playerId) => {
@@ -42,45 +49,50 @@ class App extends Component {
       })
       .catch(err => console.log(err))
   }
-  getChartData = (playerId) => {
-    var labelArr = [];
-    var dataArr = [];
-    fetch(`https://www.balldontlie.io/api/v1/stats?seasons[]=2019&player_ids[]=${playerId}&start_date='2020-07-30'&end_date='2020-08-14'`)
-    .then(res => res.json())
-    .then(data => {
-      for(let i = 0; i < data.data.length; i++){
-        labelArr.push(data.data[i]["game"]["date"])
-        dataArr.push(data.data[i]["pts"])
-      }
-      })
-    .catch(err => console.log(err))
-    this.setState({
-      chartData: {
-        key: Date.now(),
-        labels: labelArr,
-        datasets:[
-          {
-            label:"Points",
-            fill: false,
-            lineTension: 0.5,
-            data: dataArr,
-            borderColor: "rgba(0,0,0,1)",
-            backgroundColor:[
-              'rgba(255,99,132,0.6)',
-              'rgba(54,162,235,0.6)',
-              'rgba(255,206,86,0.6)',
-              'rgba(75,192,192,0.6)',
-              'rgba(153,159,64,0.6)',
-              'rgba(255,99,132,0.6)'
-            ]
-          }
-        ]
-      }
-    })
-  }
+  // getChartData = (playerId) => {
+  //   var labelArr = [];
+  //   var ptsArr = [];
+  //   var astArr = [];
+  //   var rebArr = [];
+  //   fetch(`https://www.balldontlie.io/api/v1/stats?seasons[]=2019&player_ids[]=${playerId}&start_date='2020-07-30'&end_date='2020-08-14'`)
+  //   .then(res => res.json())
+  //   .then(data => {
+  //     for(let i = 0; i < data.data.length; i++){
+  //       labelArr.push(data.data[i]["game"]["date"])
+  //       ptsArr.push(data.data[i]["pts"])
+  //       astArr.push(data.data[i]["ast"])
+  //       rebArr.push(data.data[i]["reb"])
+  //     }
+  //   })
+  //   .catch(err => console.log(err))
+  //   this.setState({
+  //     chartData: {
+  //       key: Date.now(),
+  //       labels: labelArr,
+  //       datasets:[
+  //         {
+  //           label:"Points",
+  //           fill: false,
+  //           lineTension: 0.5,
+  //           data: ptsArr,
+  //           borderColor: "rgba(34, 167, 240, 1)",
+  //           backgroundColor:[
+  //             'rgba(255,99,132,0.6)',
+  //             'rgba(54,162,235,0.6)',
+  //             'rgba(255,206,86,0.6)',
+  //             'rgba(75,192,192,0.6)',
+  //             'rgba(153,159,64,0.6)',
+  //             'rgba(255,99,132,0.6)'
+  //           ]
+  //         }
+  //       ]
+  //     }
+  //   })
+  // }
   handleSubmit = (e) => {
     e.preventDefault();
     this.getPlayerId()
+    console.log(this.state.playerKey)
     console.log(this.state.playerName)
     console.log(this.state.playerStats)
     console.log(this.state.chartData)
@@ -94,8 +106,110 @@ class App extends Component {
       alert("Please type players name")
     }
   }
+  handleChange = (e, newData) => {
+    e.preventDefault()
+    this.setState({
+      [newData.name]: newData.value
+    })
+    console.log(newData.value)
+    fetch(`https://www.balldontlie.io/api/v1/stats?seasons[]=2019&player_ids[]=${this.state.playerKey.id}&start_date='2020-07-30'&end_date='2020-08-14'`)
+    .then(res => res.json())
+    .then(data => {
+      var labelArr = [];
+      var ptsArr = [];
+      var astArr = [];
+      var rebArr = [];
+      for(let i = 0; i < data.data.length; i++){
+        labelArr.push(data.data[i]["game"]["date"])
+        ptsArr.push(data.data[i]["pts"])
+        astArr.push(data.data[i]["ast"])
+        rebArr.push(data.data[i]["reb"])
+      }
+      if(newData.value === 1){
+        this.setState({
+          chartData: {
+            key: Date.now(),
+            labels: labelArr,
+            datasets:[
+              {
+                label:"Points",
+                fill: false,
+                lineTension: 0.5,
+                data: ptsArr,
+                borderColor: "rgba(34, 167, 240, 1)",
+                backgroundColor:[
+                  'rgba(255,99,132,0.6)',
+                  'rgba(54,162,235,0.6)',
+                  'rgba(255,206,86,0.6)',
+                  'rgba(75,192,192,0.6)',
+                  'rgba(153,159,64,0.6)',
+                  'rgba(255,99,132,0.6)'
+                ]
+              }
+            ]
+          }
+        })
+      }
+      else if(newData.value === 2){
+        this.setState({
+          chartData: {
+            key: Date.now(),
+            labels: labelArr,
+            datasets:[
+              {
+                label:"Assists",
+                fill: false,
+                lineTension: 0.5,
+                data: astArr,
+                borderColor: "rgba(34, 167, 240, 1)",
+                backgroundColor:[
+                  'rgba(255,99,132,0.6)',
+                  'rgba(54,162,235,0.6)',
+                  'rgba(255,206,86,0.6)',
+                  'rgba(75,192,192,0.6)',
+                  'rgba(153,159,64,0.6)',
+                  'rgba(255,99,132,0.6)'
+                ]
+              }
+            ]
+          }
+        })
+      }
+      else if(newData.value === 3){
+        this.setState({
+          chartData: {
+            key: Date.now(),
+            labels: labelArr,
+            datasets:[
+              {
+                label:"Rebounds",
+                fill: false,
+                lineTension: 0.5,
+                data: rebArr,
+                borderColor: "rgba(34, 167, 240, 1)",
+                backgroundColor:[
+                  'rgba(255,99,132,0.6)',
+                  'rgba(54,162,235,0.6)',
+                  'rgba(255,206,86,0.6)',
+                  'rgba(75,192,192,0.6)',
+                  'rgba(153,159,64,0.6)',
+                  'rgba(255,99,132,0.6)'
+                ]
+              }
+            ]
+          }
+        })
+      }
+    })
+    .catch(err => console.log(err))
+  }
 
   render() { 
+    const options = [
+      {key: 1, text: "PTS", value:1},
+      {key: 2, text: "AST", value:2},
+      {key: 3, text: "REB", value:3},
+    ]
     return (  
       <div className="App">
         <div className="header">StatFinder</div>
@@ -161,12 +275,23 @@ class App extends Component {
         </div>
         <div className="chart">
           <h3>Bubble Game Stats</h3>
+          <div className="dropdown">
+          <Menu compact>
+            <Dropdown 
+              name="selectedStat"
+              options={options}
+              value={this.state.value}
+              simple item
+              onChange={this.handleChange}
+            />
+          </Menu>
+          </div>
         <Line
           data={this.state.chartData}
           key={this.state.chartData.key}
-          width={100}
-          height={100}
-          options={{responsive:true}}
+          width={250}
+          height={250}
+          options={{responsive:true, maintainAspectRatio:false}}
         />
         </div>
       </div>
